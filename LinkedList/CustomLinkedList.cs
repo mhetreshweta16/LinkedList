9d4bef334be25e3d3221ab9e0cd1a16b6dde0cac
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-10-25 21:40:02 +0530

    [

--- LinkedList/CustomLinkedList.cs
+++ LinkedList/CustomLinkedList.cs
@@ -9,7 +9,11 @@ namespace LinkedList
     class CustomLinkedList
     {
         public Node head;
-        public void InsertLast(int new_data)
+        /// <summary>
+        /// Inserts the node to the last.
+        /// </summary>
+        /// <param name="new_data">The new data.</param>
+       public void InsertLast(int new_data)
         {
             Node new_node = new Node(new_data);
             if (this.head == null)
@@ -22,9 +26,13 @@ namespace LinkedList
                 last_node.next = new_node;
                   
             }
-           // Console.WriteLine("Node is Inserted in List");
+            Console.WriteLine("Node is Inserted in List"+new_node.data);
         }
 
+        /// <summary>
+        /// Gets the last node.
+        /// </summary>
+        /// <returns></returns>
         public Node getLastNode()
         {
             Node temp = this.head;
@@ -36,6 +44,11 @@ namespace LinkedList
             return temp;
         }
 
+        /// <summary>
+        /// Inserts the node to the first.
+        /// </summary>
+        /// <param name="new_data">The new data.</param>
+        /// <returns></returns>
         public void InsertFirst(int new_data)
         {
 
@@ -49,12 +62,58 @@ namespace LinkedList
 
 
         }
-
+        /// <summary>
+        /// Appends the specified new data.
+        /// </summary>
+        /// <param name="new_data">The new data.</param>
+        /// <returns></returns>
         public void Append(int new_data)
         {
 
             InsertLast(new_data);
             Console.WriteLine("inserted into list" + new_data);
+        }
+        /// <summary>
+        /// Specifics the postition.
+        /// </summary>
+        /// <param name="position">The position.</param>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
+        public void specificPostition(int position, int data)
+        {
+            if (position < 1)
+            {
+                Console.WriteLine("invalid position");
+            }
+            if (position == 1)
+            {
+                Node newNode = new Node(data);
+                newNode.next = this.head;
+                this.head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Node node = new Node(data);
+                        node.next = this.head.next;
+                        head.next = node;
+                        break;
+                       
+                    }
+                
+                }
+                if (position != 1)
+                {
+                    Console.WriteLine("position is out of range");
+                }
+
+            }
+            
+
+
         }
         public void display()
         {
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -8,21 +8,25 @@ namespace LinkedList
         {
             Console.WriteLine("Hello World!");
             CustomLinkedList list = new CustomLinkedList();
+            list.InsertLast(50);
+            list.InsertLast(70);
+            Console.WriteLine("after insert node at specific position");
+            list.specificPostition(2, 30);
+            list.display();
 
-            list.Append(50);
+            //Append the node
+            /*list.Append(50);
             list.Append(30);
             list.Append(70);
             Console.WriteLine("After appending the node the list is:");
-            list.display();
-
-
+            list.display();*/
 
             //Insert First
             /*list.InsertFirst(70);
             list.InsertFirst(30);
             list.InsertFirst(50);
             Console.WriteLine("after the inserting node the list is:");*/
-            
+
         }
     }
 }
